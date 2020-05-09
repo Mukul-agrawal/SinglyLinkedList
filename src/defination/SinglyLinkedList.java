9642ce94a9cfffc1a6d2009d0e3f244d19918da2
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-09 14:11:32 +0530

    create constructor

--- src/defination/SinglyLinkedList.java
+++ src/defination/SinglyLinkedList.java
@@ -28,5 +28,13 @@ public class SinglyLinkedList<E> implements SinglyLinkedListADT<E> {
         private E data;
         private Node<E> next;
 
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node(E data) {
+            this.data = data;
+        }
     }
 }
