28d2b01597c9c47c202e2c27fbd30c40f693ba6e
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-09 14:23:47 +0530

    create method getNode

--- src/defination/SinglyLinkedList.java
+++ src/defination/SinglyLinkedList.java
@@ -6,6 +6,20 @@ public class SinglyLinkedList<E> implements SinglyLinkedListADT<E> {
     private Node<E> head = null;
     private int size = 0;
 
+    private Node<E> getNode(int index) {
+        Node<E> response = null;
+        if (index > 0 || index >= size) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        } else {
+            Node<E> node = head;
+            for (int i = 0; i < index && node != null; i++) {
+                node = node.getNext();
+            }
+            response = node;
+        }
+        return response;
+    }
+
 
     @Override
     public void add(E item) {
@@ -29,6 +43,15 @@ public class SinglyLinkedList<E> implements SinglyLinkedListADT<E> {
 
     private static class Node<E> {
         private E data;
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
         private Node<E> next;
 
         private Node(E data, Node<E> next) {
