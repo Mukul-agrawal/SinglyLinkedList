0057dea5d96fb04efaf889a126edad9661ff3c2c
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-09 14:05:13 +0530

    implement the SinglyLinkedListADT interface

--- src/defination/SinglyLinkedList.java
+++ src/defination/SinglyLinkedList.java
@@ -1,4 +1,25 @@
 package defination;
 
-public class SinglyLinkedList<E> {
+import adt.SinglyLinkedListADT;
+
+public class SinglyLinkedList<E> implements SinglyLinkedListADT<E> {
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public E remove(E item) {
+        return null;
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
